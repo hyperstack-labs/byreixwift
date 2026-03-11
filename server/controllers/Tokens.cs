48e3c5c1a7360334ac857447c3d2b0a11ec7b2bb
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-03-11 23:28:30 +0800

    feat: added Folder for API Controllers

---
+++ server/controllers/Tokens.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Tokens : ControllerBase
+    {
+        // Get: api/tokens/all
+        [HttpGet("all")]
+        
+    }
+}
