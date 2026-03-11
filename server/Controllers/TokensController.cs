75094de12b8e2bb5d6a665751b768a6609cd35ed
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-03-11 23:38:43 +0800

    refactor: renamed Controller name

---
+++ server/Controllers/TokensController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TokensController : ControllerBase
+    {
+        // Get: api/tokens/all
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllTokens()
+        {
+            Console.WriteLine("Reach API Endpoint : GET /api/tokens/all");
+            return Ok(new { message = "API Endpoint reached Successfully! " });
+        }
+        
+    }
+}
