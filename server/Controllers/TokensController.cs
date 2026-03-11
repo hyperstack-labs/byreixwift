535ce2f887cf9c74ffd5d2324653cb02a2e9c75c
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-03-11 23:40:39 +0800

    feat: added api endpoint for fetching token by single id

--- server/Controllers/TokensController.cs
+++ server/Controllers/TokensController.cs
@@ -8,10 +8,16 @@ namespace server.Controllers
     {
         // Get: api/tokens/all
         [HttpGet("all")]
-        public async Task<IActionResult> GetAllTokens()
+        public async Task<IActionResult> FetchAllTokens()
         {
             Console.WriteLine("Reach API Endpoint : GET /api/tokens/all");
-            return Ok(new { message = "API Endpoint reached Successfully! " });
+            return Ok(new { message = "API Endpoint fetching all tokens reached Successfully! " });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> FetchTokenById(string id)
+        {
+            return Ok(new { message = "API Endpoint for fetching single token by id reached Successfully! " });
         }
         
     }
