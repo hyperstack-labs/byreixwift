f02cf80e38dd1eac13f72c4385a4cbce984997df
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-03-12 00:25:57 +0800

    feat: added a Model for lightweight token price

---
+++ server/Models/TokenPrice.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace server.Models
+{
+    public class TokenPrice
+    {
+        public int Id { get; set; } = 1;
+
+        public string? Symbol { get; set; } = "SDA";
+
+        public double Price { get; set; } = 0.0;
+
+        public double PriceChangePercent24h { get; set; } = 23.5;
+
+        public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
+    }
+}
