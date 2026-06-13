e611fc5555bac19cfd688b76ae4714a681d00117
Stephen01623 <ralphjoedg@gmail.com>
2026-06-13 23:52:25 +0800

    feat: add viem contract service and escrow read functions

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,8 @@ import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
 import { EscrowsModule } from "./escrows/escrows.module";
 import { DbModule } from "./db/db.module";
+import { ContractModule } from "./contracts/contract.module";
 @Module({
-  imports: [AuthModule, EscrowsModule, DbModule],
+  imports: [AuthModule, EscrowsModule,ContractModule, DbModule],
 })
 export class AppModule {}
